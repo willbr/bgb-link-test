f5316a7b1aabc5b11f97211cc1ab2d63644df9d9
William Bettridge-Radford <william.bettridge.radford@gail.com>
2021-11-02 23:51:40 +0000

    playing with message buffer

--- term.py
+++ term.py
@@ -7,13 +7,19 @@ from enum import IntEnum
 import socket
 
 
+sock = None
+timestamp = 0
+remote_timestamp = 0
+
+
 class bgb_cmd(IntEnum):
-    version = 1
-    joypad  = 101
-    sync1   = 104
-    sync2   = 105
-    sync3   = 106
-    status  = 108
+    version = 1   # 0x01
+    joypad  = 101 # 0x65
+    sync1   = 104 # 0x68
+    sync2   = 105 # 0x69
+    sync3   = 106 # 0x6a
+    status  = 108 # 0x6c
+    want    = 109 # 0x6d
 
 
 class joy(IntEnum):
@@ -34,14 +40,17 @@ class BGBMessage:
     b3:  int = 0
     b4:  int = 0
 
+
     def __repr__(self):
         return "asdf"
+
 
     def __str__(self):
         if self.cmd == bgb_cmd.version:
             return f"version {self.b2}.{self.b3}"
         elif self.cmd == bgb_cmd.joypad:
-            return f"joypad {str(joy(self.b2))}"
+            pressed = self.b2 & 0b1000
+            return f"joypad {str(joy(self.b2))} {pressed=}"
         elif self.cmd == bgb_cmd.sync1:
             return f"sync1 ${self.b2:x}"
         elif self.cmd == bgb_cmd.sync2:
@@ -60,11 +69,6 @@ class BGBMessage:
             return f"unknown: ${cmd:x} ${b2:x} ${b3:x} ${b4:x}"
 
 
-sock = None
-timestamp = 0
-remote_timestamp = 0
-
-
 def init():
     global sock
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -74,12 +78,30 @@ def init():
 
 def send(msg):
     data = pack("<BBBBI", msg.cmd, msg.b2, msg.b3, msg.b4, timestamp)
-    print(' s', msg)
+    if msg.cmd not in [bgb_cmd.sync3]:
+        print('s', msg)
     cnt = sock.send(data)
     assert cnt == 8
 
 
-def send_msg(cmd, b2, b3, b4):
+def send_msg(cmd, b2=None, b3=None, b4=None):
+
+    if cmd == bgb_cmd.sync1:
+        if b3 == None:
+            b3 = 0x81
+    elif cmd == bgb_cmd.sync1:
+        if b2 == None:
+            b2 = 0x55
+        if b3 == None:
+            b3 = 0x80
+
+    if b2 == None:
+        b2 = 0
+    if b3 == None:
+        b3 = 0
+    if b4 == None:
+        b4 = 0
+
     send(BGBMessage(cmd, b2, b3, b4))
 
 
@@ -92,25 +114,36 @@ def recv():
 
     *reply, remote_timestamp = unpack("<BBBBI", data)
     msg = BGBMessage(*reply)
-    if msg.cmd not in [bgb_cmd.sync3]:
-        print('r', msg)
+    if msg.cmd not in [bgb_cmd.sync3, bgb_cmd.joypad]:
+        print(' r', msg)
 
     return msg
-
-
-def parse_stdio(msg):
-    return BGBMessage(bgb_cmd.sync1, 0xff, 0b1000_0001, 0)
 
 
 def link_client(conn):
     global timestamp
 
     init()
+    msgs = []
+    buffer = []
+    ready = True
+
     while True:
+
         if conn.poll():
-            msg = conn.recv()
-            send(parse_stdio(msg))
-            #conn.send(['hi', msg])
+            print(f"{buffer=} {msgs=} {ready=}")
+            line = conn.recv()
+            print(f"{line=}")
+            msgs.append(line)
+
+        if buffer and ready:
+            c = buffer.pop(0)
+            print(f"{c=} {buffer=} {msgs=} {ready=}")
+            send_msg(bgb_cmd.sync1, c)
+            ready = False
+        elif len(buffer) == 0 and msgs:
+            line = msgs.pop(0)
+            buffer = list(line.encode()) + [0]
 
         msg = recv()
         if msg == None:
@@ -125,30 +158,29 @@ def link_client(conn):
         elif cmd == bgb_cmd.status:
             send(msg)
         elif cmd == bgb_cmd.sync1:
-            pass
+            send_msg(bgb_cmd.sync2)
         elif cmd == bgb_cmd.sync2:
-            conn.send('oi')
-            pass
+            ready = True
         elif cmd == bgb_cmd.sync3:
-            if b2 == 0:
+            if msg.b2 == 0:
                 timestamp = remote_timestamp
+            send(msg)
         else:
-            print('r', cmd, b2, b3, b4)
+            print('r', msg)
             assert False, cmd
-
-        timestamp = remote_timestamp
 
 
 def main():
     parent_conn, child_conn = Pipe()
     p = Process(target=link_client, args=(child_conn,))
     p.start()
-    print(parent_conn.recv())
+
     while True:
         line = input("# ")
         parent_conn.send(line)
-        print(parent_conn.recv())
-    # p.join()
+
+    p.join()
+
 
 if __name__ == '__main__':
     main()
